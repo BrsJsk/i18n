671c4c60d9e2d6137b053838b73794d4d66763f4
Zeke Sikelianos <zeke@sikelianos.com>
2020-03-22 15:19:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const walk = require('walk-sync')
 const { nodeVersions } = require('../package.json')
 const nodeMajors = Object.keys(nodeVersions)
 const contentDir = path.join(__dirname, 'content')
-const pages = walk(contentDir, {directories: false})
+const allPages = walk(contentDir, { directories: false })
   .filter(filename => filename.endsWith('.md'))
   .map(filename => {
     const fullPath = path.join(contentDir, filename)
     const nodeVersion = filename.split(path.sep)[0]
     const locale = filename.split(path.sep)[1]
-    const filePath = filename.split(path.sep).slice(2).join(path.sep)
-    return {locale, nodeVersion, filePath, fullPath}
+    const filePath = filename
+      .split(path.sep)
+      .slice(2)
+      .join(path.sep)
+    return { locale, nodeVersion, filePath, fullPath }
   })
 
 module.exports = {
-  pages,
-  getDocset
+  allPages,
+  getPages
 }
 
-async function getDocset(nodeMajor, locale) {
+async function getPages (nodeMajor, locale) {
   // set defaults
   nodeMajor = nodeMajor || nodeMajors[0] // latest
   locale = locale || 'en-US'
 
-  return pages
+  return allPages
     .filter(page => page.nodeVersion === nodeMajor)
     .filter(page => page.locale === locale)
 }
